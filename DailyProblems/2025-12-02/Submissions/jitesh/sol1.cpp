4d30728a3b521fc6174a3ecb8e465eee6399dab5
jitesh59 <jiteshnayak70@gmail.com>
2025-12-03 10:28:36 +0530

    jitesh_dec_02 solution

---
+++ DailyProblems/2025-12-02/Submissions/jitesh/sol1.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+
+        int x = 0, y = 1, z;
+        for(int i = 2; i <= n; i++) {
+            z = x + y;
+            x = y;
+            y = z;
+        }
+        return y;
+    }
+};
